821b18bd8b88364a8ad8ef35a898f94ca1c76dcd
Marcin Lulek <info@webreactor.eu>
2020-03-28 22:55:06 +0100

    fix FRONTEND_ASSSET_ROOT_DIR

--- backend/ziggurat_cms/lib/cli.py
+++ backend/ziggurat_cms/lib/cli.py
@@ -17,7 +17,7 @@ def build_assets(registry, asset_config, *cmd_args, **cmd_kwargs):
     shutil.copytree(asset_config['asset_path'], dest,
                     ignore=shutil.ignore_patterns(
                         'node_modules', 'bower_components', '__pycache__'))
-    os.environ['ZIGGURAT_CMS_STATIC_DIR'] = settings['static.dir']
+    os.environ['FRONTEND_ASSSET_ROOT_DIR'] = settings['static.dir']
     subprocess.check_output(['yarn'], env=os.environ, cwd=dest)
     subprocess.check_output(['node_modules/.bin/webpack'], env=os.environ,
                             cwd=dest)
